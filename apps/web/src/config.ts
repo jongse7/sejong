70412b8f8713dc693343fe1c069a3e5aab8d05d1
jongse7 <mmssjj72@naver.com>
2026-03-05 11:07:23 +0900

    chore(web): use sejong.png as default share image

--- apps/web/src/config.ts
+++ apps/web/src/config.ts
@@ -4,7 +4,7 @@ export const SITE = {
   profile: "https://github.com/jongse7",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
   title: "SEJONG",
-  ogImage: "astropaper-og.jpg",
+  ogImage: "sejong.png",
   lightAndDarkMode: false,
   postPerIndex: 4,
   postPerPage: 4,
@@ -16,7 +16,7 @@ export const SITE = {
     text: "Edit page",
     url: "https://github.com/satnaing/astro-paper/edit/main/",
   },
-  dynamicOgImage: true,
+  dynamicOgImage: false,
   dir: "ltr", // "rtl" | "auto"
   lang: "en", // html lang code. Set this empty and default will be "en"
   timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
